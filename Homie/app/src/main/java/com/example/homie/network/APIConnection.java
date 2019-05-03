adf7de1680b40c709d43619c96ecba281740c8df
denis <denisramiros@gmail.com>
2019-05-03 10:07:00 +0200

    Add firebase-auth dependency

--- Homie/app/src/main/java/com/example/homie/network/APIConnection.java
+++ Homie/app/src/main/java/com/example/homie/network/APIConnection.java
@@ -1,5 +1,8 @@
 package com.example.homie.network;
 
+import android.support.annotation.NonNull;
+import android.util.Log;
+
 import com.example.homie.DTO.UserLoginDTO;
 import com.example.homie.DTO.UserRegisterDTO;
 import com.example.homie.network.retrofit.LoginRequest;
@@ -7,8 +10,18 @@ import com.example.homie.network.retrofit.MovementRequest;
 import com.example.homie.network.retrofit.RegisterRequest;
 import com.example.homie.viewModels.AuthCallBack;
 import com.example.homie.viewModels.MovementSensorCallBack;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class APIConnection implements NetworkConnection {
+
+    private FirebaseAuth auth;
+
+    public APIConnection(){
+        auth = FirebaseAuth.getInstance();
+    }
 
     @Override
     public void loginAccount(String email, String password, AuthCallBack viewModel) {
@@ -17,7 +30,17 @@ public class APIConnection implements NetworkConnection {
 
     @Override
     public void createAccount(String firstName, String lastName, String email, String password, AuthCallBack viewModel) {
-        new RegisterRequest().start(new UserRegisterDTO(firstName, lastName, email, password), viewModel);
+        auth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    String userId = auth.getCurrentUser().getUid();
+                    Log.d("FirebaseRegisterUID",""+userId);
+                    //new RegisterRequest().start(new UserRegisterDTO(firstName, lastName, email, password), viewModel);
+                }
+            }
+        });
+
     }
 
     @Override
