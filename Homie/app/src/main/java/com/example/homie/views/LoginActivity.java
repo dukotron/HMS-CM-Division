fb3d67a34a071c1fdd5d162b3dc0cb4a93aa383f
denis <denisramiros@gmail.com>
2019-05-09 11:05:56 +0200

    Update login UI

--- Homie/app/src/main/java/com/example/homie/views/LoginActivity.java
+++ Homie/app/src/main/java/com/example/homie/views/LoginActivity.java
@@ -9,6 +9,7 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.example.homie.DRO.SensorDRO;
@@ -22,6 +23,7 @@ public class LoginActivity extends AppCompatActivity{
 
     EditText email;
     EditText password;
+    ProgressBar progressBar;
 
     LoginViewModel viewModel;
 
@@ -32,11 +34,10 @@ public class LoginActivity extends AppCompatActivity{
 
         email = findViewById(R.id.emailLogin);
         password = findViewById(R.id.passwordLogin);
+        progressBar = findViewById(R.id.progressBar_login);
         
         initLoginButton();
-
         initViewModel();
-
     }
 
     void initViewModel() {
@@ -46,6 +47,7 @@ public class LoginActivity extends AppCompatActivity{
             @Override
             public void onChanged(@Nullable Boolean aBoolean) {
                 if (aBoolean) {
+                    progressBar.setVisibility(View.GONE);
                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
                     finish();
                 }
@@ -56,6 +58,7 @@ public class LoginActivity extends AppCompatActivity{
             @Override
             public void onChanged(@Nullable Boolean aBoolean) {
                 if (!aBoolean) {
+                    progressBar.setVisibility(View.GONE);
                     email.setError("Wrong Email");
                 }
             }
@@ -65,6 +68,7 @@ public class LoginActivity extends AppCompatActivity{
             @Override
             public void onChanged(@Nullable Boolean aBoolean) {
                 if (!aBoolean) {
+                    progressBar.setVisibility(View.GONE);
                     password.setError("Wrong Password");
                 }
             }
@@ -74,6 +78,7 @@ public class LoginActivity extends AppCompatActivity{
             @Override
             public void onChanged(@Nullable Boolean aBoolean) {
                 if (aBoolean) {
+                    progressBar.setVisibility(View.GONE);
                     Toast.makeText(LoginActivity.this, "Some error occurred!", Toast.LENGTH_LONG).show();
                 }
             }
@@ -85,19 +90,11 @@ public class LoginActivity extends AppCompatActivity{
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                progressBar.setVisibility(View.VISIBLE);
                 viewModel.loginUser(email.getText().toString().trim(), password.getText().toString().trim());
             }
         });
     }
-//
-//    void testMovRequest() {
-//        String token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjM4MDEwNjVlNGI1NjNhZWVlZWIzNTkwOTEwZDlmOTc3YTgxMjMwOWEiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vaG9taWUtYjAxYzYiLCJhdWQiOiJob21pZS1iMDFjNiIsImF1dGhfdGltZSI6MTU1NzEyNjI4MCwidXNlcl9pZCI6ImJFVTBsdHkxNGJldmZrRm9pWFVJZkNHQW9KMzIiLCJzdWIiOiJiRVUwbHR5MTRiZXZma0ZvaVhVSWZDR0FvSjMyIiwiaWF0IjoxNTU3MTI2MjgwLCJleHAiOjE1NTcxMjk4ODAsImVtYWlsIjoidGVzdEB0ZXN0LnRlc3QiLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsidGVzdEB0ZXN0LnRlc3QiXX0sInNpZ25faW5fcHJvdmlkZXIiOiJwYXNzd29yZCJ9fQ.B1UqGjW4ycV7WQIuojkLGHxVZ_UqwDhdv2YTT4RV5aFuwBGWRjKslme8GpvXzHL48wkDvBF7HuzaBlj46fXXIbYc3A4oWgEk4fR6aFBd5eK74uJlSU-FgDVsWoXJeB7dzrmlWWxAA2qPQaj90MM8ggxQikin9yTpHR6al9qRAAMOtsQ7jl3-bm9Zc97FghQVcpGw9LQ911daR_8siV40NdXR8dZTS2V3keoNtZgsnfiSrJqcAx6zdEfGUI5y6AYUj8uf37xXMSm9-c3A3gZUFdUAVBsa9N3aesIqi_Bda4DpnA1heJHYu9I9rFkWY70qqjdQjowqLP0rLEnam-GiAQ";
-//        String userId = "bEU0lty14bevfkFoiXUIfCGAoJ32";
-//        String from = "2017-09-08T19:01:55.714942";
-//        String to = "2019-09-08T19:01:55.714942";
-//        new MovementRequest().start(token, userId, this, from, to);
-//    }
-
 
     public void createAccountActivity(View view) {
         Intent intent = new Intent(this, RegisterActivity.class);
